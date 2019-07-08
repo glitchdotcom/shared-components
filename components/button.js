386761aaec680c785422c926ff21cd4ae275df6d
Glitch (hello-express) <none>
2019-07-08 14:12:06 +0000

    💈🍲 Checkpoint ./components/button.js:271885/485 ./watch.json:271885/1 ./package.json:271885/3

--- components/button.js
+++ components/button.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const PlainButton = styled.button`
+const ResetButton = styled.button`
   appearance: none;
   background-color: transparent;
   border: 0;
+  border-radius: 0;
+  padding: 0;
+  margin: 0;
+  font-family: inherit;
+  font-size: 100%;
+  line-height: 1.15;
+  text-transform: none;
+`;
+
+export const TransparentButton = styled(ResetButton)`
+  display: block;
+  width: 100%;
+`;
+
+const Button = styled(ResetButton)`
   
-`
+`;
