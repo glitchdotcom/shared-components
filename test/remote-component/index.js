83c3fea079b73ab1bc66dc6b9bbdd8561475763b
Glitch (hello-express) <none>
2019-07-10 22:15:31 +0000

    🎲🗽 Checkpoint ./lib/stories.js:271885/288 ./lib/system.js:271885/287 ./lib/index.js:271885/69 ./test/remote-component/index.html:271885/148 ./test/remote-component/index.js:271885/41 ./package.json:271885/16

--- test/remote-component/index.js
+++ test/remote-component/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled';
-import { lightTheme, createRemoteComponent } from '@glitch/components'
+import { lightTheme, RootStyle, createRemoteComponent } from '@glitch/components'
 
 const Button = createRemoteComponent('https://power-passenger.glitch.me/module.js', 'Button')
 const onClick = () => console.log('clicked')
@@ -9,6 +9,7 @@ const onClick = () => console.log('clicked')
 const AppContainer = () => (
   <ThemeProvider theme={lightTheme}>
     <main>
+      <RootStyle />
       <h1>Test remote component</h1>
       <Button onClick={onClick}>Click me</Button>
     </main>
