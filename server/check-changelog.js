901e82b9403860aa74f41f283fb4001dba680979
Glitch (power-passenger) <none>
2019-07-31 22:00:03 +0000

    😹🐦 Checkpoint ./server/check-changelog.js:271885/224 ./package.json:271885/312

--- server/check-changelog.js
+++ server/check-changelog.js
@@ -1,15 +1,17 @@
 const fs = require('fs').promises;
-const path = require('path')
-const { exec } = require('child_process')
+const path = require('path');
 const { version } = require('../package.json');
 
-const CHANGELOG_PATH = path.resolve(__dirname, '../changelog.md')
+const CHANGELOG_PATH = path.resolve(__dirname, '../changelog.md');
 
 const check = async () => {
-  const file = fs.readFile(CHANGELOG_PATH, 'utf8')
-  const [mostRecentUpdate] = file.match(/---\s+##\s+\[([\d\.]+)\]/)
-  console.log({ mostRecentUpdate })
-  if (version !== mostRecentUpdate) process.exit(1)
-}
+  const file = await fs.readFile(CHANGELOG_PATH, 'utf8');
+  const [, mostRecentUpdate] = file.match(/---\s+##\s+\[([^\]]+)\]/);
+  console.log({ mostRecentUpdate });
+  if (version !== mostRecentUpdate) {
+    console.error(`Most recent changelog entry is for version ${mostRecentUpdate}; expected entry for ${version}`);
+    process.exit(1);
+  }
+};
 
-check()
+check();
