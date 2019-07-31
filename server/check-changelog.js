c14f6adb1365bfc16a57a315997c82ac739e4f24
Glitch (power-passenger) <none>
2019-07-31 22:00:44 +0000

    version

--- server/check-changelog.js
+++ server/check-changelog.js
@@ -7,7 +7,6 @@ const CHANGELOG_PATH = path.resolve(__dirname, '../changelog.md');
 const check = async () => {
   const file = await fs.readFile(CHANGELOG_PATH, 'utf8');
   const [, mostRecentUpdate] = file.match(/---\s+##\s+\[([^\]]+)\]/);
-  console.log({ mostRecentUpdate });
   if (version !== mostRecentUpdate) {
     console.error(`Most recent changelog entry is for version ${mostRecentUpdate}; expected entry for ${version}`);
     process.exit(1);
