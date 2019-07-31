ab0c978f37878fbe62c4a7a6246cd6290722c56a
Glitch (power-passenger) <none>
2019-07-31 20:54:56 +0000

    ✍️🍣 Checkpoint ./server/changelog.js:271885/1076 ./changelog.md:271885/11

--- server/changelog.js
+++ server/changelog.js
@@ -6,11 +6,30 @@ const exec = promisify(childProcess.exec);
 const { version } = require('../package.json');
 
 const CHANGELOG_PATH = path.resolve(__dirname, '../changelog.md')
+const ignoredFiles = ['lib/index.js', 'lib/stories.js', 'changelog.md', 'package.json']
+
 const getGitDiff = () => exec("git diff origin/master --name-status");
 
-const insertChange = async () => {
-  const fileText = await fs.readFile(CHANGELOG_PATH)
-  
+const parseDiff = (diff) => 
+  diff.split('\n')
+    .map(line => {
+      const [changeType, file] = line.split(/\s/, 1)
+      return { changeType, file } // TODO: these need to be 'resolve'-d
+    })
+
+const formatDiff = (parsedDiff) => {
+  const filtered = parsedDiff(({ file }) => !ignoredFiles.includes(file));
+  const grouped = { added: [], removed: [], modified: []}
+  for (const change of filtered) {
+    if (changeType === 'A') {}
+  }
+}
+
+const insertChanges = async () => {
+  const fileText = await fs.readFile(CHANGELOG_PATH, 'utf8')
+  const [head, tail] = fileText.split('---', 1)
+  const joined = `${head}---\n${fileText}\n${tail}`
+  await fs.writeFile(CHANGELOG_PATH, joined, 'utf8')
 }
 
 async function main () {
