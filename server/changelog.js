834a817b6179977f132c391d394783422d7d9054
Glitch (power-passenger) <none>
2019-07-31 21:38:03 +0000

    🎃👀 Checkpoint ./server/changelog.js:271885/349 ./changelog.md:271885/1723

--- server/changelog.js
+++ server/changelog.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process')
 const { version } = require('../package.json');
 
 const CHANGELOG_PATH = path.resolve(__dirname, '../changelog.md')
-const ignoredFiles = ['lib/index.js', 'lib/stories.js', 'changelog.md', 'package.json']
+const ignoredFiles = ['lib/index.js', 'lib/stories.js', 'changelog.md', 'package.json', 'package-lock.json']
 
 const getGitDiff = () => new Promise((resolve) => {
   exec("git diff origin/master --name-status", (err, stdout, stderr) => {
@@ -15,9 +15,9 @@ const getGitDiff = () => new Promise((resolve) => {
 const parseDiff = (diff) => {
   const grouped = { Added: [], Removed: [], Changed: []}
 
-  const lines = diff.split('\n')
+  const lines = diff.trim().split('\n')
   for (const line of lines) {
-    const [changeType, file] = line.split(/\s/, 1)
+    const [changeType, file] = line.split(/\s+/)
     if (ignoredFiles.includes(file)) continue;
     // https://git-scm.com/docs/git-diff#Documentation/git-diff.txt---diff-filterACDMRTUXB82308203
     if (changeType === 'A') {
@@ -45,21 +45,17 @@ const formatDiff = (parsedDiff) => {
   return lines.join('\n')
 }
 
-const insertChanges = async () => {
+const insertChanges = async (changes) => {
   const fileText = await fs.readFile(CHANGELOG_PATH, 'utf8')
-  const [head, tail] = fileText.split('---', 1)
-  const joined = `${head}---\n${fileText}\n${tail}`
-  console.log('joined', joined)
+  const [head, tail] = fileText.split(/---\n/)
+  const joined = `${head}---\n\n${changes}\n${tail}`
   await fs.writeFile(CHANGELOG_PATH, joined, 'utf8')
 }
 
 async function main () {
   const diff = await getGitDiff()
-  console.log('diff', diff)
   const parsedDiff = parseDiff(diff)
-  console.log('parsedDiff', parsedDiff)
   const changelogText = formatDiff(parsedDiff)
-  console.log('changelogText', changelogText)
   await insertChanges(changelogText)
 }
 
