d36c1088b540e851e54d131f51fece863bf2636a
Cassey Lottman <>
2020-03-04 16:10:42 -0600

    lint all the things

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,37 +7,16 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:jsx-a11y/recommended', 'plugin:react/recommended'],
   parser: 'babel-eslint',
+  ignorePatterns: ['node_modules/', 'server/', 'test/', 'sh/', 'test/', 'build/'],
   rules: {
-    // Overrides/additions to eslint:recommended:
-    'no-else-return': ERROR,
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    semi: ['error', 'always'],
-    'no-debugger': WARN,
-    'no-console': OFF,
-    'no-restricted-syntax': [OFF],
-    'arrow-parens': [ERROR, 'always'],
-    'operator-linebreak': [ERROR, 'after', { overrides: { '?': 'before', ':': 'before' } }],
-    // disabled for prettier compatibility
-    'implicit-arrow-linebreak': OFF,
-    'object-curly-newline': OFF,
-    'no-confusing-arrow': OFF,
-    'function-paren-newline': OFF,
-    'max-len': [
-      'error',
-      150,
-      2,
-      {
-        ignoreUrls: true,
-        ignoreComments: true,
-        ignoreRegExpLiterals: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-      },
-    ],
-    'no-param-reassign': [OFF],
-    'no-mixed-operators': OFF,
-  }
+    'react/no-unescaped-entities': OFF,
+    'react/prop-types': OFF /* TODO this would be nice */,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
