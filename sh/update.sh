44b59f10be44724d8fb6b6fb410fbe7da0c7528e
Cassey Lottman <>
2020-03-19 14:25:39 -0500

    clean up & comment

--- sh/update.sh
+++ sh/update.sh
@@ -3,8 +3,7 @@
 # pass an argument if you want to pull a different branch name than the current branch you're on
 # no argument needed to update against the branch name of the current branch
 
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
-
+# branch to use is the first argument, but if the first argument was not passed, use the current git branch
 branchToUse="$1" && [[  -z "$1" ]]  && branchToUse="$(git rev-parse --abbrev-ref HEAD)"
 
 git checkout $branchToUse
